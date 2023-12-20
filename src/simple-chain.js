1397495a868dd3babefa8ecc4aa86930814a5bcc
Volha Patciahevich <zaprudskaya@gmail.com>
2023-12-20 17:18:08 +0300

    feat: solve next 2 tasks

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -5,25 +5,35 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 const chainMaker = {
+  arr : [],
   getLength() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+   return this.arr.length
   },
-  addLink(/* value */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  addLink(value) {
+    if (value === undefined) {
+      value = ''
+    }
+    this.arr.push(`( ${value} )`)
+    return this
   },
-  removeLink(/* position */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  removeLink(position) {
+    if (typeof(position) === 'number' && position > 0 && position % 1 === 0 && position <= this.arr.length) {
+      this.arr.splice(position - 1, 1)
+      return this
+    } else {
+      this.arr = []
+      throw new Error(`You can't remove incorrect link!`)
+    }
+    
   },
   reverseChain() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+   this.arr.reverse()
+   return this
   },
   finishChain() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    const result = this.arr.join('~~')
+    this.arr = []
+    return result
   }
 };
 
